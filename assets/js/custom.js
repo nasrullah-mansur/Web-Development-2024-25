a8371dce9a7e1beed38aa3617951610b18cba395
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-08-11 21:01:44 +0600

    class-21

---
+++ assets/js/custom.js
@@ -0,0 +1,51 @@
+
+
+
+// document.write('Hello world');
+
+// console.log('this is my console area.');
+
+// var myH1 = document.getElementById('test').innerHTML;
+
+
+// document.getElementById('test').innerHTML = 'this is from javascript';
+
+ 
+
+// var person1 = 100;
+// var person2 = 200;
+
+
+// var myH1 = 'another hello';
+
+// console.log(myH1);
+
+let price = 10;
+
+
+var test1 = 'this is variable one';
+let test2 = 'this is variable two';
+const TEST3 = 'this is variable three';
+
+
+
+
+test1 = 'this is re variable one';
+var test1 = 'this is re build variable one';
+
+
+test2 = 'this is re test two';
+test2 = 'this is something';
+
+
+
+
+
+console.log(test2);
+
+
+
+
+
+
+

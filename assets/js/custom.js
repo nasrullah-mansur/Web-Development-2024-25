4b24386d141fe7ea84fa7487a1039434d7246a41
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-08-14 21:56:37 +0600

    class 24

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,162 +1,172 @@
-let test1 = document.getElementById('demo');
+// Arithmetic Operators
 
-let test2 = 'this is test';
-
-let firstName = 'nasrullah';
-let lastName = 'mansur';
+let a = 10;
+let b = 20;
 
 
-// console.dir(a);
+let output = a + b;
 
-let b = [
-    firstName,
-    lastName,
-    'hello',
-    [1,5,'hello', 'something', 'kisu ekta'],
-    null,
-    undefined,
-    true,
-];
+output = b - a;
+
+output = b / a;
+
+output = 2 ** 5;
+
+output = 10 % 3;
+
+output = 100;
+
+// output ++;
+// output ++;
+// output ++;
+// output ++;
+// output++;
+
+// output = output + 10;
+
+// output -= 10;
+
+let c = 10;
+let d = 100;
+
+let e = 'hello';
+let f = 'world';
+
+output = c >= d; // true;
+
+output = e + ' ' + f;
+
+// output = output + ' kisu ekta';
+output += ' kisu ekta';
+
+
+output = 10 == 10 || 20 == 200;
+
+
+// 5 ** 5;
+
+
+// output = 10 * 10 / (2 + 3) - 5;
 
 
 
-let person = [
-    'something',
-    'junayed',
-    'ahmad',
-    14,
-    'class 5',
-    'section 2',
-    'dhaka',
-];
+// if (10 >= 10) {
 
+//     console.log('mile gece');
 
-let student = {
-    something: 'something',
-    firstName: lastName,
-    secondName: 'ahmad',
+// }
+
+// else {
+//   console.log('mile nai');
     
-    myArr: [1,2,3,4,5],
-    section: 'section 2',
-    address: 'dhaka',
-    age: 14,
+// }
+
+
+// let age = 200;
+
+// if(age > 80) {
+//     console.log('tumi bura hoye geco');
+// }
+
+// else if(age >= 18) {
+//     console.log('tumi boro hoye geco');
+// }
+
+// else {
+//     console.log('tumi ekhono picci');
+// }
+
+
+let day = 0;
+// day = new Date().getDay();
+// let today = 'test';
+let today;
+
+// console.log(day);
+
+
+// if(day == 0) {
+//     today = 'Sunday';
+// }
+// else if(day == 1) {
+//     today = 'Monday';
+// }
+// else if(day == 2) {
+//     today = 'Tuesday';
+// }
+// else if(day == 3) {
+//     today = 'Wednesday';
+// }
+// else if(day == 4) {
+//     today = 'Thursday';
+// }
+// else if(day == 5) {
+//     today = 'Friday';
+// }
+// else if(day == 6) {
+//     today = 'Saturday';
+// }
+
+// else {
+//     console.log('vai vulval number dio na');
+    
+// }
+
+
+// switch (day) {
+//     case 0:
+//         today = 'Sunday';
+//         break;
+
+//     case 1:
+//         today = 'Monday';
+//         break;
+
+
+//     default:
+//         console.log('test');
+        
+//         break;
+// }
+
+// console.log('Today is: ' + today);
+
+let studentOne = {
+    name: 'Rashed',
+    bangla: 50,
+    english: 70
 }
 
-// console.log(person);
+let studentTwo = {
+    name: 'zayed',
+    bangla: 10,
+    english: 20
+}
 
-let arr = student.myArr;
+let result = studentTwo;
 
-console.log( arr[3] );
+let count = (result.bangla + result.english) / 2;
+let finalResult;
 
+if(count > 90) {
+    finalResult = "A+";
+}
 
+else if(count > 80) {
+    finalResult = "A"
+}
 
 
+let aa = {one: 'one', two: 'two'};
+let bb = {one: 'one', two: 'two'};
 
+output = aa == bb;
 
+console.log(output);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// something 
-// console.log(person[2]);
-
-console.log(student.address);
 
 
 

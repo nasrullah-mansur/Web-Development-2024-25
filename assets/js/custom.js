88c46fd8727fc031594ab33720f9bd72d7bfbe86
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-08-16 20:55:36 +0600

    class 25

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,172 +1,148 @@
-// Arithmetic Operators
+
 
 let a = 10;
-let b = 20;
+let b = 810;
 
+let output = a == b;
 
-let output = a + b;
-
-output = b - a;
-
-output = b / a;
-
-output = 2 ** 5;
-
-output = 10 % 3;
-
-output = 100;
-
-// output ++;
-// output ++;
-// output ++;
-// output ++;
-// output++;
-
-// output = output + 10;
-
-// output -= 10;
-
-let c = 10;
-let d = 100;
-
-let e = 'hello';
-let f = 'world';
-
-output = c >= d; // true;
-
-output = e + ' ' + f;
-
-// output = output + ' kisu ekta';
-output += ' kisu ekta';
-
-
-output = 10 == 10 || 20 == 200;
-
-
-// 5 ** 5;
-
-
-// output = 10 * 10 / (2 + 3) - 5;
-
-
-
-// if (10 >= 10) {
-
-//     console.log('mile gece');
-
+// if(output) {
+//     console.log('yes');
+// } else {
+//     console.log('no');
 // }
 
-// else {
-//   console.log('mile nai');
+
+// let newOutput = a == b ? 'yes' : 'no';
+// let newOutput = a == b && 'yes';
+// let newOutput = a == b || 'yes';
+// let newOutput = a == b ?? true;
+// let newOutput = a == b ?? 'yes';
+
+// Truthy and Falsy Values
+
+// Truthy values
+// 1. string;
+// 2. number;
+// 3. object;
+// 4. array;
+// 5. true;
+
+
+// falsy value;
+// 1. false;
+// 2. null;
+// 3. undefined;
+// 4. 0;
+// 5. '';
+
+
+let newOutput = false;
+
+let output2 = !123;
+
+
+// console.log(output2);
+
+
+let product = {
+    productName: 'mobile',
+    price: 25,
+}
+
+
+// if(product.productName == undefined) {
+//     console.log('Please choose a product first'); 
+// } else {
+//     console.log('Give me product price now;');
+// }
+
+// test();
+
+
+function test() {
+    console.log('this is from function');
+}
+
+
+
+
+
+function test2() {
+    let gg = 100;
+
+    console.log(a);
     
-// }
+}
 
 
-// let age = 200;
-
-// if(age > 80) {
-//     console.log('tumi bura hoye geco');
-// }
-
-// else if(age >= 18) {
-//     console.log('tumi boro hoye geco');
-// }
-
-// else {
-//     console.log('tumi ekhono picci');
-// }
 
 
-let day = 0;
-// day = new Date().getDay();
-// let today = 'test';
-let today;
+// test2();
 
-// console.log(day);
+// student() = result;
 
 
-// if(day == 0) {
-//     today = 'Sunday';
-// }
-// else if(day == 1) {
-//     today = 'Monday';
-// }
-// else if(day == 2) {
-//     today = 'Tuesday';
-// }
-// else if(day == 3) {
-//     today = 'Wednesday';
-// }
-// else if(day == 4) {
-//     today = 'Thursday';
-// }
-// else if(day == 5) {
-//     today = 'Friday';
-// }
-// else if(day == 6) {
-//     today = 'Saturday';
-// }
+function student(s_name, s_bangla, s_english) {
+    let name = s_name;
+    let bangla = s_bangla;
+    let english = s_english;
 
-// else {
-//     console.log('vai vulval number dio na');
+    let total = bangla + english;
+    // let output = (total / 2) >= 33 ? 'Pass' : 'Fail';
+
+    let finalOutput = (total / 2) >= 33 ? name + ' is passed' : name + ' is failed';  
+
+    // console.log(finalOutput);
+
+    let aa = 10 == 10;
+
+    return finalOutput;
+}
+
+// 'jayed is passed';
+
+// student('Zayed', 20, 50)
+
+console.log( student('Zayed', 20, 50) );
+
+document.write(student('Zayed', 20, 50));
+
+let myResult = student('Zayed', 20, 50);
+
+// student('Khaled', 20, 20);
+// student('rashed', 20, 20);
+
+
+
+function student2(test = 'something') {
+    console.log(test);
     
+}
+
+
+// student2('nothing');
+
+
+
+
+let student3 = function () {
+    console.log('this is anonymous function');
+}
+
+let student4 = () => {
+    console.log('this is arrow function');
+}
+
+// student3();
+// student4();
+
+
+
+
+// student('name', 30, 40);
+
+// {
+//     name: 'name',
+//     english: 20,
+//     bangla: 20
 // }
-
-
-// switch (day) {
-//     case 0:
-//         today = 'Sunday';
-//         break;
-
-//     case 1:
-//         today = 'Monday';
-//         break;
-
-
-//     default:
-//         console.log('test');
-        
-//         break;
-// }
-
-// console.log('Today is: ' + today);
-
-let studentOne = {
-    name: 'Rashed',
-    bangla: 50,
-    english: 70
-}
-
-let studentTwo = {
-    name: 'zayed',
-    bangla: 10,
-    english: 20
-}
-
-let result = studentTwo;
-
-let count = (result.bangla + result.english) / 2;
-let finalResult;
-
-if(count > 90) {
-    finalResult = "A+";
-}
-
-else if(count > 80) {
-    finalResult = "A"
-}
-
-
-let aa = {one: 'one', two: 'two'};
-let bb = {one: 'one', two: 'two'};
-
-output = aa == bb;
-
-console.log(output);
-
-
-
-
-
-
-

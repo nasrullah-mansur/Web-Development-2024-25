0e9a6972aade4d471c8ff05987fb9aebbce3cbab
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-08-17 21:07:26 +0600

    class-26

---
+++ assets/js/backup.js
@@ -0,0 +1,121 @@
+// abc() // ei rokom ekta function ase;
+// aa = undefined; // ei rokom ekta variable ase;
+
+// types of function
+
+// function a() {
+
+// }
+
+// let a = () => {
+
+// }
+
+
+// let a = function() {
+
+// }
+
+
+// (function() {
+//     console.log('this is auto run function');
+// })();
+
+// (() => {
+//     console.log('this is auto run function');
+// })();
+
+
+
+// function a(name, english, math) {
+//     let test = {
+//         name: name,
+//         english: english,
+//         math: math,
+//     }
+
+//     // let test = [1,2,3,4];
+
+//     return test;
+// }
+
+// console.log( a('zayed', 30, 20) );
+
+
+
+
+// function mainFunction(myVar, myFun) {
+//     console.log(myVar + myFun());
+// }
+
+// let hello = 'Hello ';
+
+let world = function() {
+    return 'World';
+};
+
+
+// // mainFunction(hello, world);
+// mainFunction(hello, function() {
+//                     return 'World';
+//                 });
+
+
+
+// let a = 0;
+// let timeFun = () => {
+//     console.log(a++);
+// }
+
+
+// // setTimeout(function() {
+// //     console.log('this is setTimeOut Function');
+// // }, 2000);
+
+// setInterval(timeFun, 1000);
+
+
+// let test1 = 10;
+// const test4 = 20;
+// var abdfasd= 3456;
+
+// var abc = 1000;
+
+
+// if(true) {
+//     var abc = 10;
+// }
+
+
+// console.log(abc);
+
+
+// abc();
+
+// function abc() {
+//     console.log('hello');
+    
+// }
+
+// console.log(aa);
+
+
+// var aa = 'kisu ekta';
+
+
+
+// var aa;
+// aa = 'kisu ekta';
+
+// console.log(aa);
+
+// var aa = 'kisu ekta';
+
+// console.log(x);
+
+// x = 10;
+// const x;
+
+
+
+// x name ekta variable ase jar value undefined;
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,148 +1,34 @@
-
-
-let a = 10;
-let b = 810;
-
-let output = a == b;
-
-// if(output) {
-//     console.log('yes');
-// } else {
-//     console.log('no');
-// }
-
-
-// let newOutput = a == b ? 'yes' : 'no';
-// let newOutput = a == b && 'yes';
-// let newOutput = a == b || 'yes';
-// let newOutput = a == b ?? true;
-// let newOutput = a == b ?? 'yes';
-
-// Truthy and Falsy Values
-
-// Truthy values
-// 1. string;
-// 2. number;
-// 3. object;
-// 4. array;
-// 5. true;
-
-
-// falsy value;
-// 1. false;
-// 2. null;
-// 3. undefined;
-// 4. 0;
-// 5. '';
-
-
-let newOutput = false;
-
-let output2 = !123;
-
-
-// console.log(output2);
-
-
-let product = {
-    productName: 'mobile',
-    price: 25,
-}
-
-
-// if(product.productName == undefined) {
-//     console.log('Please choose a product first'); 
-// } else {
-//     console.log('Give me product price now;');
-// }
-
-// test();
-
-
-function test() {
-    console.log('this is from function');
-}
+    let hoursElement = document.getElementById('hours');
+    let minutesElement = document.getElementById('minutes');
+    let secondsElement = document.getElementById('seconds');
+    let ampmElement = document.getElementById('ampm');
 
 
 
+function ourDigitalClock() {
 
-
-function test2() {
-    let gg = 100;
-
-    console.log(a);
+    let now = new Date();
+    let hoursNumber = (now.getHours()) > 12 ? (now.getHours()) - 12 : (now.getHours());
+    let minutesNumber = now.getMinutes();
+    let secondsNumber = now.getSeconds();
     
+    let ampmString = (now.getHours()) > 12 ? 'PM' : 'AM';
+
+    hoursElement.innerHTML = hoursNumber < 10 ? '0' + hoursNumber : hoursNumber;
+    minutesElement.innerHTML = minutesNumber < 10 ? '0' + minutesNumber : minutesNumber;
+    secondsElement.innerHTML = secondsNumber < 10 ? '0' + secondsNumber : secondsNumber;
+    ampmElement.innerHTML = ampmString;
 }
 
 
 
-
-// test2();
-
-// student() = result;
-
-
-function student(s_name, s_bangla, s_english) {
-    let name = s_name;
-    let bangla = s_bangla;
-    let english = s_english;
-
-    let total = bangla + english;
-    // let output = (total / 2) >= 33 ? 'Pass' : 'Fail';
-
-    let finalOutput = (total / 2) >= 33 ? name + ' is passed' : name + ' is failed';  
-
-    // console.log(finalOutput);
-
-    let aa = 10 == 10;
-
-    return finalOutput;
-}
-
-// 'jayed is passed';
-
-// student('Zayed', 20, 50)
-
-console.log( student('Zayed', 20, 50) );
-
-document.write(student('Zayed', 20, 50));
-
-let myResult = student('Zayed', 20, 50);
-
-// student('Khaled', 20, 20);
-// student('rashed', 20, 20);
-
-
-
-function student2(test = 'something') {
-    console.log(test);
-    
-}
-
-
-// student2('nothing');
+setInterval(ourDigitalClock, 1000);
 
 
 
 
-let student3 = function () {
-    console.log('this is anonymous function');
-}
-
-let student4 = () => {
-    console.log('this is arrow function');
-}
-
-// student3();
-// student4();
 
 
 
 
-// student('name', 30, 40);
 
-// {
-//     name: 'name',
-//     english: 20,
-//     bangla: 20
-// }

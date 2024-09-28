fe3b6215b296868027ed2eb56c7192e2a347763d
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-09-28 20:59:57 +0600

    class-37

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,198 +1,82 @@
 
 
-// 1. registration;
-// 2. course choose;
-// 3. make payment;
-// 4. keep learning;
-// 5. complete course;
-// 6. take exam;
-// 7. take certificate;
+let myH1 = document.querySelector('h1');
+let myH1jquery = $('h1');
+
+
+console.log(myH1, myH1jquery);
 
 
 
 
-// function registration(callback) {
-//     console.log('Log in successful');
-//     callback();
-// }
-
-// function courseChoose(callback) {
-//     let choose = false;
-//     if(choose) {
-//         console.log('Please choose your favorite course');
-//         callback();
-//     }
-
-// }
-
-// function makePayment(callback) {
-//     console.log('Please pay first');
-
-//     callback();
-// }
-
-// function keepLearning(callback) {
-//     setTimeout(() => {
-//         console.log('Well done! your payment is successful, Please keep learning.');
-
-//         callback();
-//     }, 2000);
-// }
-
-// function completeCourse(callback) {
-//     console.log('Well done! you have completed your course.');
-
-//     callback();
-// }
-
-// function takeExam(callback) {
-//     let result = 'fail';
-//     if(result != 'fail') {
-//         console.log('Well done! you have passed your exam.');
-
-//         callback();
-//     } else {
-//         console.log('Sorry! you are failed. Please take exam again.');
-//     }
-// }
-
-// function takeCertificate() {
-//     console.log('Your certificate is ready. Please contact us.');
-// }
 
 
 
-// registration();
-// courseChoose();
-// makePayment();
-// keepLearning();
-// completeCourse();
-// takeExam();
+// let myLis = document.querySelectorAll('li');
 
-
-// registration(function() {
-//     courseChoose(function() {
-//         makePayment(function() {
-//             keepLearning(function() {
-//                 completeCourse(function() {
-//                     takeExam(function() {
-//                         takeCertificate();
-//                     });
-//                 });
-//             });
-//         });
-//     });
+// Array.from(myLis).forEach(element => {
+//     element.innerHTML = 'One Test';
 // });
 
 
 
 
-// ********************
-// *************************** Promises *********************
+$(document).ready(function() {
 
-function registration() {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            console.log('Log in successful');
-            resolve();
-        }, 2000)
+
+    // // jQuery;
+    // jQuery('li').html('One Test');
+
+    $('h1').click(function() {
+        $('h1').hide();
     });
 
-}
+    // $('button').click(function() {
+    //     $('h1').show();
+    // });
 
-function courseChoose() {
-    
-    return new Promise(function(resolve) {
-        console.log('Please choose your favorite course');
-        resolve();
-    });
-
-}
-
-function makePayment() {
-
-    return new Promise(function(resolve) {
-        setTimeout(() => {
-            console.log('Please pay first');
-            resolve();
-        }, 2000);
-    });
-
-}
-
-function keepLearning(callback) {
-    setTimeout(() => {
-        console.log('Well done! your payment is successful, Please keep learning.');
-
-        callback();
-    }, 2000);
-}
-
-function completeCourse(callback) {
-    console.log('Well done! you have completed your course.');
-
-    callback();
-}
-
-function takeExam(callback) {
-    let result = 'fail';
-    if(result != 'fail') {
-        console.log('Well done! you have passed your exam.');
-
-        callback();
-    } else {
-        console.log('Sorry! you are failed. Please take exam again.');
-    }
-}
-
-function takeCertificate() {
-    console.log('Your certificate is ready. Please contact us.');
-}
+    // document.querySelector('h1').addEventListener('click', function() {
+    //     document.querySelector('h1').style.display = 'none';
+    // })
 
 
-
-
-
-// let myPromise = new Promise(function(resolve) {
-
-//     resolve('Promise rakha somvob hosse na');
-// });
-
-// console.log(myPromise);
-
-// registration()
-//     .then(courseChoose)
-//     .then(makePayment)
-
-
-
-let myArr = null;
-
-function first() {
-
-    return new Promise(function(resolve) {
-        setTimeout(() => {
-            myArr = [1,2,3,4,5];
-    
-            resolve();
-        }, 2000);
+    $('.toggle').click(function() {
+        $('.box').toggle();
     })
 
-}
+    $('.slideUp').click(function() {
+        $('.box').slideUp();
+    })
 
-function second() {
-    let newArr = myArr.map(function(n) {
-        return n + 5;
-    });
+    $('.slideDown').click(function() {
+        $('.box').slideDown();
+    })
 
-    myArr = newArr;
+    $('.slideToggle').click(function() {
+        let test = 'test';
+        $('.box').slideToggle();
+    })
 
-    console.log(myArr);
-}
+    $('.fadeUp').click(function() {
+        $('.box').fadeIn();
+    })
+
+    $('.fadeDown').click(function() {
+        $('.box').fadeOut();
+    })
 
 
+});
 
-first()
-    .then(second)
+
+document.querySelectorAll('li').innerHTML = 'sofasdhfa;sdhf ';
+// $('li:first-child').html('somehaosdjfp asdfpoa ');
+
+document.querySelector('li').html('test');
+
+// console.log(lis);
+// console.log(lisjquery);
+
+
 
 

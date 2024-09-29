d9466de9a5bb15d9cefc3d03b506c7d9c251b5d4
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-09-29 20:18:54 +0600

    class-28

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,82 +1,110 @@
+// =========== jQuery Methods =============
 
 
-let myH1 = document.querySelector('h1');
-let myH1jquery = $('h1');
+// .show();
+// .hide();
+// .toggle();
+// .slideUp();
+// .slideDown();
+// .slideToggle();
+// .fadeIn();
+// .fadeOut();
+// .fadeToggle();
+// .addClass();
+// .removeClass();
+// .toggleClass();
+
+// $('h1').addClass('test hello another');
 
 
-console.log(myH1, myH1jquery);
+// Events;
+// .click();
 
-
-
-
-
-
-
-// let myLis = document.querySelectorAll('li');
-
-// Array.from(myLis).forEach(element => {
-//     element.innerHTML = 'One Test';
+// $('.fadeToggle').click(function() {
+//     $('.box').fadeToggle();
 // });
 
+// $('.fadeToggle').on('click', function() {
+//     $('.box').fadeToggle();
+// })
+
+// $('#myInput').on('focus', function() {
+//     let a = '#myInput';
+
+//     $(this).val('this is something');
+// })
+
+// $('#myInput').on('blur', function() {
+//     let value = $('#myInput').val();
+
+//     if(value == '') {
+//         $('#myInput').val('kicu ekta likhun');
+//     }
+// })
 
 
 
-$(document).ready(function() {
+// Getter & Setter;
+// .html();
+// let myText = $('h1').text();
+// let myHtml = $('h1').html();
+// let myValue = $('input').val();
+// let myType = $('input').attr('type');
+
+// $('h1').text('another text');
+// $('h1').html('<span>another text</span>');
+// $('input').val('this is my value form javascript');
+// $('input').attr('type', 'password');
+
+// $('.my-btn').on('click', function() {
+//     let btnText  = $(this).text();
+//     let inputText = $('input').attr('type');
+
+//     if(btnText == 'Show') {
+//         $(this).text('Hide');
+//     } else {
+//         $(this).text('Show');
+//     }
+
+//     if(inputText == 'password') {
+//         $('input').attr('type', 'text');
+//     } else {
+//         $('input').attr('type', 'password');
+//     }
+// })
 
 
-    // // jQuery;
-    // jQuery('li').html('One Test');
+// $('h1').append('<p>this is p tag</p>');
+// $('h1').prepend('<p>this is p tag</p>');
+// $('h1').after('<p>this is p tag</p>');
+// $('h1').before('<p>this is p tag</p>');
 
-    $('h1').click(function() {
-        $('h1').hide();
+
+// $('h1').remove();
+// $('h1').empty();
+
+// let h1Color = $('h1').css('color');
+
+// $('h1').css({
+//     color: 'yellow',
+//     backgroundColor: 'red',
+// });
+
+// $('span').parentUntil('li');
+
+// $('h1').find('strong');
+// $('h1 strong')
+
+
+
+$(document).ready(function(){
+    $('.slider-container').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        slidesToShow: 2,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>'
     });
-
-    // $('button').click(function() {
-    //     $('h1').show();
-    // });
-
-    // document.querySelector('h1').addEventListener('click', function() {
-    //     document.querySelector('h1').style.display = 'none';
-    // })
-
-
-    $('.toggle').click(function() {
-        $('.box').toggle();
-    })
-
-    $('.slideUp').click(function() {
-        $('.box').slideUp();
-    })
-
-    $('.slideDown').click(function() {
-        $('.box').slideDown();
-    })
-
-    $('.slideToggle').click(function() {
-        let test = 'test';
-        $('.box').slideToggle();
-    })
-
-    $('.fadeUp').click(function() {
-        $('.box').fadeIn();
-    })
-
-    $('.fadeDown').click(function() {
-        $('.box').fadeOut();
-    })
-
-
 });
-
-
-document.querySelectorAll('li').innerHTML = 'sofasdhfa;sdhf ';
-// $('li:first-child').html('somehaosdjfp asdfpoa ');
-
-document.querySelector('li').html('test');
-
-// console.log(lis);
-// console.log(lisjquery);
-
-
-
-

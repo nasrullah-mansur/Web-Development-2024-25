73cbe5a841beb2a9173a5e46f64b57711ce065f0
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-10-01 20:24:44 +0600

    jquery plugins

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,100 +1,6 @@
 // =========== jQuery Methods =============
 
 
-// .show();
-// .hide();
-// .toggle();
-// .slideUp();
-// .slideDown();
-// .slideToggle();
-// .fadeIn();
-// .fadeOut();
-// .fadeToggle();
-// .addClass();
-// .removeClass();
-// .toggleClass();
-
-// $('h1').addClass('test hello another');
-
-
-// Events;
-// .click();
-
-// $('.fadeToggle').click(function() {
-//     $('.box').fadeToggle();
-// });
-
-// $('.fadeToggle').on('click', function() {
-//     $('.box').fadeToggle();
-// })
-
-// $('#myInput').on('focus', function() {
-//     let a = '#myInput';
-
-//     $(this).val('this is something');
-// })
-
-// $('#myInput').on('blur', function() {
-//     let value = $('#myInput').val();
-
-//     if(value == '') {
-//         $('#myInput').val('kicu ekta likhun');
-//     }
-// })
-
-
-
-// Getter & Setter;
-// .html();
-// let myText = $('h1').text();
-// let myHtml = $('h1').html();
-// let myValue = $('input').val();
-// let myType = $('input').attr('type');
-
-// $('h1').text('another text');
-// $('h1').html('<span>another text</span>');
-// $('input').val('this is my value form javascript');
-// $('input').attr('type', 'password');
-
-// $('.my-btn').on('click', function() {
-//     let btnText  = $(this).text();
-//     let inputText = $('input').attr('type');
-
-//     if(btnText == 'Show') {
-//         $(this).text('Hide');
-//     } else {
-//         $(this).text('Show');
-//     }
-
-//     if(inputText == 'password') {
-//         $('input').attr('type', 'text');
-//     } else {
-//         $('input').attr('type', 'password');
-//     }
-// })
-
-
-// $('h1').append('<p>this is p tag</p>');
-// $('h1').prepend('<p>this is p tag</p>');
-// $('h1').after('<p>this is p tag</p>');
-// $('h1').before('<p>this is p tag</p>');
-
-
-// $('h1').remove();
-// $('h1').empty();
-
-// let h1Color = $('h1').css('color');
-
-// $('h1').css({
-//     color: 'yellow',
-//     backgroundColor: 'red',
-// });
-
-// $('span').parentUntil('li');
-
-// $('h1').find('strong');
-// $('h1 strong')
-
 
 
 $(document).ready(function(){
@@ -107,4 +13,49 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>'
     });
+
+  
+
+    $.ajax({
+        method: 'GET',
+        header: 'application/json',
+        url: 'https://course.divinecoder.com/food-categories',
+        success: (res) => {
+            console.log(res);
+            
+        },
+        error: (error) => {
+            console.log(error);
+            
+        }
+    });
+
+
+    let myobj = {
+        my_name: 'your name',
+        business_email: 'your email',
+        subject: 'your subject',
+    }
+
+
+    $.ajax({
+        method: 'POST',
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        data: myobj,
+        success: () => {},
+        error: () => {},
+    });
+
+    $.get('https://jsonplaceholder.typicode.com/posts', function(res) {
+        console.log(res);
+    });
+
+
+    var mixer = mixitup('.gallery-container');
+
+
+    new VenoBox({
+        selector: ".my-image-links"
+    });
+
 });

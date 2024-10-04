4f4316af009689385da00456a620c29b5b938ad2
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-10-04 20:30:04 +0600

    react js

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+let Li = () => {
+  return <li>This is li</li>
+}
+
+
+
+
+function App() {
+  return <ul>
+    <Li></Li>
+    <Li></Li>
+    <Li></Li>
+    <Li></Li>
+    <Li></Li>
+  </ul>
+}
+
+export default App;

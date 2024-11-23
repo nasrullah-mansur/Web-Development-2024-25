f204a17354707a0221d3b78e83e023d2862c0a4b
nasrullah-mansur <nasrullah.cit.bd@gmail.com>
2024-11-23 19:13:23 +0600

    react query end

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,4 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
 export default function CreateUser() {
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationKey: ["users"],
+        mutationFn: (variables) => {
+            return axios.post("http://localhost:3000/users", variables);
+        },
+        onSuccess: (data, variables, context) => {
+            console.log(data, variables, context);
+            queryClient.invalidateQueries(["users"]);
+        },
+        onMutate: (variables) => {
+            return `Hello ${variables.name}. How are you?`;
+        },
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const newData = Object.fromEntries(formData);
+
+        mutation.mutate({
+            ...newData,
+            id: crypto.randomUUID(),
+        });
+    };
+
     return (
         <div className="w-3/12">
             <div className="p-2 border">
@@ -6,7 +37,7 @@ export default function CreateUser() {
                     Create a new user
                 </h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label
                             htmlFor="name"
@@ -17,6 +48,7 @@ export default function CreateUser() {
                         <input
                             type="text"
                             id="name"
+                            name="name"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="John"
                             required
@@ -33,6 +65,7 @@ export default function CreateUser() {
                         <input
                             type="text"
                             id="username"
+                            name="username"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="John"
                             required
@@ -49,6 +82,7 @@ export default function CreateUser() {
                         <input
                             type="email"
                             id="email"
+                            name="email"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="John"
                             required
@@ -56,7 +90,7 @@ export default function CreateUser() {
                     </div>
 
                     <button
-                        type="button"
+                        type="submit"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                     >
                         Save User
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -15,24 +15,33 @@ export default function UserList() {
     } = useQuery({
         queryKey: ["users"],
         queryFn: getData,
+        // retry: false,
+        // staleTime: 5000,
+        refetchInterval: 2000,
     });
 
-    if (isFetching) return <div className="w-6/12">Data is loading ...</div>;
-    if (error)
-        return <div className="w-6/12">Something error {error.message}</div>;
+    // if (isFetching) return <div className="w-6/12">Data is loading ...</div>;
+    // if (error)
+    //     return <div className="w-6/12">Something error {error.message}</div>;
 
     return (
-        <div className="w-6/12">
-            <div className="p-2 border">
-                <h1 className="text-2xl font-semibold mb-5">All Users Here</h1>
+        <>
+            {!error && (
+                <div className="w-6/12">
+                    <div className="p-2 border">
+                        <h1 className="text-2xl font-semibold mb-5">
+                            All Users Here
+                        </h1>
 
-                <div className="flex flex-wrap">
-                    {users &&
-                        users.map((user) => (
-                            <UserItem key={user.id} user={user} />
-                        ))}
+                        <div className="flex flex-wrap">
+                            {users &&
+                                users.map((user) => (
+                                    <UserItem key={user.id} user={user} />
+                                ))}
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 }
